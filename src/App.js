1fc2c33bd72bcfd0a214d4dd9e4ef3482b7698a0
KevinPNguy01 <kevinpnguy01@gmail.com>
2024-10-03 11:04:38 -0700

    Add dividers

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import { Divider } from '@mui/material';
-import './App.css';
-import { EaterEnrollment } from './components/EaterEnrollment';
-import { Intro } from './components/Intro';
-import { Languages } from './components/Lanuages';
-import { MinecraftMod } from './components/MinecraftMod';
-import { VoxelGameEngine } from './components/VoxelGameEngine';
-import EditIcon from '@mui/icons-material/Edit';
-import GpsFixedIcon from '@mui/icons-material/GpsFixed';
-import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
-import TerrainIcon from '@mui/icons-material/Terrain';
 import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
+import EditIcon from '@mui/icons-material/Edit';
 import EventIcon from '@mui/icons-material/Event';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import GpsFixedIcon from '@mui/icons-material/GpsFixed';
 import MapIcon from '@mui/icons-material/Map';
 import SearchIcon from '@mui/icons-material/Search';
-import { Project } from './components/Project';
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
+import TerrainIcon from '@mui/icons-material/Terrain';
 import TextureIcon from '@mui/icons-material/Texture';
 import TransformIcon from '@mui/icons-material/Transform';
 import ViewInArIcon from '@mui/icons-material/ViewInAr';
+import { Divider } from '@mui/material';
+import './App.css';
 import { FeatureCard } from './components/FeatureCard';
+import { Intro } from './components/Intro';
+import { Languages } from './components/Lanuages';
+import { Project } from './components/Project';
 
 function App() {
     return (
         <>
         <Intro/>
-        <Languages/>
-        <Divider/>
+        <Divider style={{backgroundColor: "#222"}}/>
         <Project 
             title="Eater Enrollment" 
             description="Search for courses and manage class schedules at UCI."
@@ -37,6 +33,9 @@ function App() {
             <FeatureCard title="Visual Interface" text="An intuitive calendar interface that supports both added courses and user-created events." img={<EventIcon style={{ color: 'white' }}/>}/>
             <FeatureCard title="Built-in Map" text="An integrated map to see where classes are and on which days." img={<MapIcon style={{ color: 'white' }}/>}/>
         </Project>
+        <div className="w-full flex justify-center">
+            <Divider style={{backgroundColor: "#444", width: "90%"}}/>
+        </div>
         <Project 
             title="Minecraft 3D Model Importer" 
             description="A Minecraft mod that allows you to import, transform, and place 3D Models into your worlds."
@@ -46,6 +45,9 @@ function App() {
             <FeatureCard title="Texture Mapping" text="Model textures are mapped by matching pixels to the visually most similar blocks." img={<TextureIcon style={{ color: 'white' }}/>}/>
             <FeatureCard title="3D Transformations" text="Translate, rotate, and scale models in-game using intuitive mouse and keyboard controls." img={<TransformIcon style={{ color: 'white' }}/>}/>
         </Project>
+        <div className="w-full flex justify-center">
+            <Divider style={{backgroundColor: "#444", width: "90%"}}/>
+        </div>
         <Project 
             title="Voxel Game Engine" 
             description="A Minecraft-inspired voxel game written in C++ and using OpenGL."
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,13 @@
+import { Languages } from "./Lanuages";
+
 export function Intro() {
     return (
-        <div className="h-full w-full justify-center flex flex-col items-center gap-4">
-            <h1 className="text-white text-6xl font-bold">Kevin Nguy</h1>
-            <p className="text-white text-3xl font-semibold">Coder for fun</p>
+        <div className="flex flex-col gap-8 mb-8 w-full">
+            <div className="h-[100vh] w-full justify-center flex flex-col items-center gap-4">
+                <h1 className="text-white text-6xl font-bold">Kevin Nguy</h1>
+                <p className="text-white text-3xl font-semibold">Coder for fun</p>
+            </div>
+            <Languages/>
         </div>
     )
 }
