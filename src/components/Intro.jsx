40bc0038ec65893228faa24e134a621277653a59
KevinPNguy01 <kevinpnguy01@gmail.com>
2024-10-04 01:00:02 -0700

    Add social media links

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,9 @@
+import { IconButton } from "@mui/material";
 import { Languages } from "./Lanuages";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 export function Intro() {
     return (
@@ -6,6 +11,20 @@ export function Intro() {
             <div className="h-[100vh] w-full justify-center flex flex-col items-center gap-4">
                 <h1 className="text-white text-6xl font-bold">Kevin Nguy</h1>
                 <p className="text-white text-3xl font-semibold">Coder for fun</p>
+                <div>
+                    <IconButton target="_blank" href="https://github.com/kevinpnguy01">
+                        <GitHubIcon style={{color: "white"}}/>
+                    </IconButton>
+                    <IconButton target="_blank" href="https://www.linkedin.com/in/knkevin/">
+                        <LinkedInIcon style={{color: "white"}}/>
+                    </IconButton>
+                    <IconButton target="_blank" href="https://www.youtube.com/knkevin/">
+                        <YouTubeIcon style={{color: "white"}}/>
+                    </IconButton>
+                    <IconButton target="_blank" href="https://www.instagram.com/kevinpnguy/">
+                        <InstagramIcon style={{color: "white"}}/>
+                    </IconButton>
+                </div>
             </div>
             <Languages/>
         </div>
