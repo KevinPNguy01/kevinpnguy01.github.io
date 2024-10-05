f5560162e62e8c09397e935b0ab90c231e20ae19
KevinPNguy01 <kevinpnguy01@gmail.com>
2024-10-05 14:40:56 -0700

    Update website icon and title

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import './output.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.title = "Kevin Nguy's Portfolio"
 root.render(
   <React.StrictMode>
     <App />
