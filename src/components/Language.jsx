7cd9aff3855ccd2f79e1b0a5a847638f32fefa3f
KevinPNguy01 <kevinpnguy01@gmail.com>
2024-10-05 11:09:31 -0700

    Add description to tech stack

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,7 +1,7 @@
 export function Language(props) {
     const {name, children} = props;
     return (
-        <div className="flex flex-col gap-2 justify-center items-center">
+        <div className="w-1/3 lg:w-fit flex flex-col gap-2 justify-center items-center">
             {children}
             <span className="text-white w-full text-center font-bold">{name}</span>
         </div>
--- src/components/Lanuages.jsx
+++ src/components/Lanuages.jsx
@@ -1,50 +1,62 @@
 import { Language } from './Language';
+import { Divider } from '@mui/material';
 
 export function Languages() {
-    const languageClasses = "h-24";
+    const languageClasses = "h-16 lg:h-24";
     
     return (
-        <div className="flex flex-wrap items-end justify-around gap-16 px-[10%]">
-            <Language name="C">
-                <img className={languageClasses} src={require('../assets/c.png')} alt=""/>
-            </Language>
-            <Language name="C++">
-                <img className={languageClasses} src={require('../assets/c++.png')} alt=""/>
-            </Language>
-            <Language name="Java">
-                <img className={languageClasses} src={require('../assets/java.png')} alt=""/>
-            </Language>
-            <Language name="Python">
-                <img className={languageClasses} src={require('../assets/python.png')} alt=""/>
-            </Language>
-            <Language name="JavaScript">
-                <img className={languageClasses} src={require('../assets/javascript.png')} alt=""/>
-            </Language>
-            <Language name="TypeScript">
-                <img className={languageClasses} src={require('../assets/typescript.png')} alt=""/>
-            </Language>
-            <Language name="HTML">
-                <img className={languageClasses} src={require('../assets/html.png')} alt=""/>
-            </Language>
-            <Language name="CSS">
-                <img className={languageClasses} src={require('../assets/css.png')} alt=""/>
-            </Language>
-            
-            <Language name="FireBase">
-                <img className={languageClasses} src={require('../assets/firebase.png')} alt=""/>
-            </Language>
-            <Language name="GraphQL">
-                <img className={languageClasses} src={require('../assets/graphql.png')} alt=""/>
-            </Language>
-            <Language name="React">
-                <img className={languageClasses} src={require('../assets/react.png')} alt=""/>
-            </Language>
-            <Language name="Redux">
-                <img className={languageClasses} src={require('../assets/redux.png')} alt=""/>
-            </Language>
-            <Language name="Tailwind CSS">
-                <img className={languageClasses} src={require('../assets/tailwind.png')} alt=""/>
-            </Language>
-        </div>
+        <>
+            <Divider style={{backgroundColor: "#222"}}/>
+            <div className="w-full flex flex-col items-center gap-2">
+                <h2 className="text-white text-4xl font-bold text-center">
+                    My Tech Stack
+                </h2>
+                <p className="w-full lg:w-[36%] px-8 text-neutral-400 text-center font-semibold text-lg">
+                    I like to work with a variety of languages and technologies. Here are the ones I'm most comfortable with.
+                </p>
+            </div>
+            <div className="flex flex-wrap items-end justify-around gap-y-8 lg:gap-x-16 px-[10%]">
+                <Language name="C">
+                    <img className={languageClasses} src={require('../assets/c.png')} alt=""/>
+                </Language>
+                <Language name="C++">
+                    <img className={languageClasses} src={require('../assets/c++.png')} alt=""/>
+                </Language>
+                <Language name="Java">
+                    <img className={languageClasses} src={require('../assets/java.png')} alt=""/>
+                </Language>
+                <Language name="JavaScript">
+                    <img className={languageClasses} src={require('../assets/javascript.png')} alt=""/>
+                </Language>
+                <Language name="TypeScript">
+                    <img className={languageClasses} src={require('../assets/typescript.png')} alt=""/>
+                </Language>
+                <Language name="Python">
+                    <img className={languageClasses} src={require('../assets/python.png')} alt=""/>
+                </Language>
+                <Language name="HTML">
+                    <img className={languageClasses} src={require('../assets/html.png')} alt=""/>
+                </Language>
+                <Language name="CSS">
+                    <img className={languageClasses} src={require('../assets/css.png')} alt=""/>
+                </Language>
+                
+                <Language name="FireBase">
+                    <img className={languageClasses} src={require('../assets/firebase.png')} alt=""/>
+                </Language>
+                <Language name="GraphQL">
+                    <img className={languageClasses} src={require('../assets/graphql.png')} alt=""/>
+                </Language>
+                <Language name="React">
+                    <img className={languageClasses} src={require('../assets/react.png')} alt=""/>
+                </Language>
+                <Language name="Redux">
+                    <img className={languageClasses} src={require('../assets/redux.png')} alt=""/>
+                </Language>
+                <Language name="Tailwind CSS">
+                    <img className={languageClasses} src={require('../assets/tailwind.png')} alt=""/>
+                </Language>
+            </div>
+        </>
     )
 }
