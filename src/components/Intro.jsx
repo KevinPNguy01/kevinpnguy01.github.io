1f174d34efa6a3a1f9bf7c66a2ebd17a36f46b05
Kevin Nguy <72185570+KevinPNguy01@users.noreply.github.com>
2025-01-20 13:25:11 -0800

    Add scroll down indicator

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,14 +1,29 @@
-import { Button, IconButton } from "@mui/material";
-import { Languages } from "./Lanuages";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import { Button, IconButton } from "@mui/material";
+import { useEffect, useState } from 'react';
+import { Languages } from "./Lanuages";
 
 export function Intro() {
+    const [opacity, setOpacity] = useState(1);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollTop = window.scrollY;
+            const windowHeight = window.innerHeight;
+            const scrollPercentage = (scrollTop / windowHeight);
+            setOpacity(1-scrollPercentage*3);
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
         <div className="flex flex-col gap-8 mb-8 w-full">
-            <div className="h-[100vh] w-full justify-center flex flex-col items-center gap-4">
+            <div className="h-screen w-full justify-center flex flex-col items-center gap-4">
                 <h1 className="text-white text-6xl font-bold">Kevin Nguy</h1>
                 <p className="text-white text-3xl font-semibold">Coder for fun</p>
                 <div>
@@ -28,6 +43,11 @@ export function Intro() {
                 <div className="grid grid-cols-2 gap-4 md:gap-8 md:w-1/3 lg:w-1/4 py-4">
                     <Button className="!rounded-full !font-semibold !border-2" variant="outlined" target="_blank" href="https://drive.google.com/file/d/17ADWxamoleud6c1fuwy3iqPqQ7d96nK0/view?usp=sharing">Resume</Button>
                     <Button className="!rounded-full bg-gradient-to-br from-blue-500 to-blue-600" variant="contained" href="mailto: kevinpnguy01@gmail.com">Hire Me</Button>
+                </div>
+                <div className='absolute animate-bounce bottom-0 pb-4' style={{opacity}}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="white" class="bi bi-chevron-compact-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67"/>
+                    </svg>
                 </div>
             </div>
             <Languages/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        bounce: "bounce 2s infinite"
+      },
+      keyframes: {
+        wiggle: {
+          "40%, 60%": {
+            transform: "translateY(0)",
+            "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)"
+          },
+          "50%": {
+            transform: "translateY(-25%)",
+            "animation-timing-function": "cubic-bezier(0, 0, 0.2, .1)"
+          }
+        }
+      }
+    },
   },
   plugins: [],
 }
